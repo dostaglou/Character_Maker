58bf3bb858ed5559eacfc3f07fa3d8c1e8d2fa2d
dillon wyatt <dostaglou@gmail.com>
2019-08-11 16:45:11 +0900

    added name to character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,24 +1,53 @@
 class CharactersController < ApplicationController
-
+  before_action :user_id, only: [:new, :create, :index]
+  before_action :set_character, only: [:show, :edit, :update, :destroy]
 
   def index
+    redirect_to user_path(@user)
   end
 
   def show
   end
 
   def new
+    @character = Character.new
   end
 
   def create
+    @character = Character.new(character_params)
+    @character.user_id = @user
+    if character.save
+      redirect_to user_character_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @character.update(character_params)
+    @character.save
+    redirect_to user_path
   end
 
   def destroy
+    @character.destroy
+    redirect_to user_path
+  end
+
+  private
+
+  def user_id
+    @user = User.find(params[:user_id])
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :description, :allies, :enemies, :race_id)
   end
 end
